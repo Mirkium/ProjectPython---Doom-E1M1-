24bf054157e8fbb94182a9fa343e32be4bdc0e4f
mkbyx <leo.caron@ynov.com>
2025-11-02 00:35:03 +0100

    base charac

---
+++ script/character/entity.py
@@ -0,0 +1,17 @@
+class entity:
+    def entity(self, name:str, speed:int, life:int):
+        self.name = name
+        self.speed = speed
+        self.life = life
+
+    def vision(self):
+        pass #def le champ visuel
+
+    def apperance(self):
+        pass #load image charac 
+
+    def getDamages(self):
+        pass #explicite lmao
+
+    def fire(self):
+        pass #same XD    peut etre joidre les deux
---
+++ script/character/player.py
@@ -0,0 +1,3 @@
+class player(entity):
+    def __init__(self, name, speed, life):
+        super.__init__(name, speed, life)
